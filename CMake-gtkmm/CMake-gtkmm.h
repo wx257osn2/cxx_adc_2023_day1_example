e2be5ab551648ac1759f1e8d759919a83526352d
I <1091761+wx257osn2@users.noreply.github.com>
2024-11-26 08:20:56 +0900

    add source codes (copied from the article)

---
+++ CMake-gtkmm/CMake-gtkmm.h
@@ -0,0 +1,10 @@
+// CMake-gtkmm.h : 標準のシステム インクルード ファイル用のインクルード ファイル、
+// または、プロジェクト専用のインクルード ファイル。
+
+#pragma once
+
+#include <iostream>
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照します。
+#include <gtkmm/window.h>
+#include <gtkmm/application.h>
